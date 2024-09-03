a5564b705df3b86b44dc87dc49b61cc02a2a7ca8
Jack Evans <jackevans24@gmail.com>
2024-09-03 18:05:21 +0100

    feat: add game controller that allows the user to spawn a ball on click

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Controllers
+{
+    public class GameController : MonoBehaviour
+    {
+        [SerializeField] private GameObject ballPrefab;
+
+        private Camera mainCamera;
+        private GameObject ball;
+
+        private void Awake()
+        {
+            mainCamera = Camera.main;
+
+            ball = Instantiate(ballPrefab);
+            ball.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonUp(0))
+                SpawnBall();
+        }
+
+        private void SpawnBall()
+        {
+            var mousePosition = Input.mousePosition;
+            var ballPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+            ballPosition.z = 0f;
+
+            ball.transform.position = ballPosition;
+            ball.SetActive(true);
+        }
+    }
+}
