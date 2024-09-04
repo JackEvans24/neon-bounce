2047ae1f998b23a4217150953eaedb15aeae9d98
Jack Evans <jackevans24@gmail.com>
2024-09-04 16:14:33 +0100

    feat: change beam colour on hit

--- Assets/Scripts/Mechanics/Beam.cs
+++ Assets/Scripts/Mechanics/Beam.cs
@@ -4,10 +4,29 @@ namespace AmuzoBounce.Mechanics
 {
     public class Beam : MonoBehaviour
     {
+        [Header("References")]
+        [SerializeField] private SpriteRenderer sprite; 
         [SerializeField] private ParticleSystem particles;
+
+        [Header("Colour")]
+        [SerializeField] private Gradient colourIntensityMap;
+        [SerializeField] private float intensityDecay;
+
+        private float spriteIntensity;
+
+        private void FixedUpdate()
+        {
+            if (spriteIntensity < float.Epsilon)
+                return;
+
+            spriteIntensity = Mathf.Max(0f, spriteIntensity - (intensityDecay * Time.fixedDeltaTime));
+            sprite.color = colourIntensityMap.Evaluate(spriteIntensity);
+        }
 
         public void Bounce()
         {
+            spriteIntensity = 1f;
+
             particles.Stop();
             particles.Play();
         }
