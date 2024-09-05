65ae8bff96f8b8bcee09f53025e45e7f0d59bbef
Jack Evans <jackevans24@gmail.com>
2024-09-05 18:40:35 +0100

    chore: remove unused class

--- Assets/Scripts/Data/RoundData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AmuzoBounce.Data
-{
-    public struct RoundData
-    {
-        public int RoundIndex;
-        public ulong TargetScore;
-        public int RoundNumber => RoundIndex + 1;
-    }
-}
