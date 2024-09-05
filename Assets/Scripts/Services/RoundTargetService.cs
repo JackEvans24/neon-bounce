f7055ae1483dbf57e6b85b3caff50a821150fcb0
Jack Evans <jackevans24@gmail.com>
2024-09-05 18:22:21 +0100

    chore: tweak target scores

--- Assets/Scripts/Services/RoundTargetService.cs
+++ Assets/Scripts/Services/RoundTargetService.cs
@@ -2,7 +2,7 @@
 {
     public static class RoundTargetService
     {
-        private static uint[] TARGET_SCORES = { 3, 10, 21, 55, 89, 101, 150, 250, 500, 1000 };
+        private static uint[] TARGET_SCORES = { 2, 3, 5, 10, 16, 23, 35, 45, 60, 89, 101, 120, 150, 190, 240, 300, 400, 600, 1000 };
 
         public static uint GetTargetScore(int roundIndex) => TARGET_SCORES[roundIndex % TARGET_SCORES.Length];
     }
