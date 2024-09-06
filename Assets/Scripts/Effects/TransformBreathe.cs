44f53cd6772b2f333be95c5ede264ad0b70b4179
Jack Evans <jackevans24@gmail.com>
2024-09-06 10:56:35 +0100

    feat: add breathe in/out effect to last position indicator

---
+++ Assets/Scripts/Effects/TransformBreathe.cs
@@ -0,0 +1,19 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace AmuzoBounce.Effects
+{
+    public class TransformBreathe : MonoBehaviour
+    {
+        [SerializeField] private float scaleDelta = 0.8f;
+        [SerializeField] private float speed = 2f;
+
+        private void Awake()
+        {
+            transform
+                .DOScale(transform.localScale * scaleDelta, speed)
+                .SetLoops(-1, LoopType.Yoyo)
+                .SetEase(Ease.InOutCubic);
+        }
+    }
+}
